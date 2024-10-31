58a76eeb5ff2b0d0c7471a093850ecdad896cdf8
José Ernesto Dávila Pantoja <josernestodavila@gmail.com>
2024-10-30 22:20:38 -0400

    Set Dynamic Version.
    - Get the version based on the git tag.

--- django_queryset_erd/__init__.py
+++ django_queryset_erd/__init__.py
@@ -1,3 +1,10 @@
+import importlib.metadata
+
 from .generator import generate_erd_from_queryset
 
-__version__ = '0.1.0'
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
